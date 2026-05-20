3c5bbc893813a5a509640b73567c4c5ec7291458
Apu Rayhan <mdapurayhan1729@gmail.com>
2026-05-20 15:02:45 +0600

    Configure app for authentication system

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,13 @@ app.use(cors());
 
 app.use(express.json());
 
+// ─── Routes import 
+const authRoutes = require('./routes/authRoutes');
+
+// ─── API Routes register
+app.use('/api/auth', authRoutes);
+ 
+
 //testing
 app.get('/', (req, res) => {
   res.json({ message: 'MediBook server চলছে! ✅' });
